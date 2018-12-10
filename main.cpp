211c83b1a1314fceb6db0ce27a84cde052fb7177
Onur Can Cakmak <occ@occ.me>
2018-12-09 22:27:36 -0500

    Clean up the example

--- main.cpp
+++ main.cpp
@@ -5,29 +5,22 @@
 #include <iomanip>
 
 int main() {
-    CCS811 s1("/dev/i2c-1", 0x5b);
-    SI7021 s2("/dev/i2c-1", 0x40);
-    BMP280 s3("/dev/i2c-1", 0x76);
+    CCS811 ccs811("/dev/i2c-1", 0x5b);
+    SI7021 si7021("/dev/i2c-1", 0x40);
+    BMP280 bmp280("/dev/i2c-1", 0x76);
 
-    for (int i = 0; i < 3; i++) {
-        s1.read_sensors();
-        auto co2 = s1.get_co2();
-        auto tvoc = s1.get_tvoc();
+    while (true) {
+        ccs811.read_sensors();
+        bmp280.measure();
 
-        auto hum = s2.measure_humidity();
-        auto temp = s2.measure_temperature();
+        std::cout << "T(Si7021): " << std::fixed << std::setprecision(2) << si7021.measure_temperature() << "°C";
+        std::cout << "\tT(BMP280): " << std::fixed << std::setprecision(2) << bmp280.get_temperature() << "°C";
+        std::cout << "\tRH: " << std::fixed << std::setprecision(2) << si7021.measure_humidity() << "%";
+        std::cout << "\tCO2: " << std::dec << ccs811.get_co2() << "ppm";
+        std::cout << "\tOC: " << std::dec << ccs811.get_tvoc() << "ppm";
+        std::cout << "\tPres: " << std::fixed << std::setprecision(2) << bmp280.get_pressure() << "hPa";
+        std::cout << std::endl;
 
-        std::cout << "T: " << std::fixed << std::setprecision(3) << temp;
-        std::cout << "\tH: " << std::fixed << std::setprecision(3) << hum;
-        std::cout << "\tC: " << std::dec << co2;
-        std::cout << "\tO: " << std::dec << tvoc << std::endl;
-
-        s3.measure();
-        auto t2 = s3.get_temperature();
-        auto pres = s3.get_temperature();
-
-        std::cout << "T2: " << std::dec << t2 << "\tP: " << pres << std::endl;
-
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
