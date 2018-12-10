c1f26cc2aae8bce03739440f439f90c8e9516e4e
Onur Can Cakmak <occ@occ.me>
2018-12-09 20:18:58 -0500

    Update example usage in main

--- main.cpp
+++ main.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
-#include <iomanip>
-
+#include "BMP280.h"
 #include "CCS811.h"
 #include "SI7021.h"
 
+#include <iomanip>
+
 int main() {
-    CCS811 s("/dev/i2c-1", 0x5b);
+    CCS811 s1("/dev/i2c-1", 0x5b);
     SI7021 s2("/dev/i2c-1", 0x40);
+    BMP280 s3("/dev/i2c-1", 0x76);
 
-    std::cout << "Serial number: 0x" << hex << s2.get_serial() << std::endl;
-    std::cout << "Firmware Revision: 0x" << hex << (int) s2.get_fw_rev() << std::endl;
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
-    while (true) {
+    for (int i = 0; i < 3; i++) {
+        s1.read_sensors();
+        auto co2 = s1.get_co2();
+        auto tvoc = s1.get_tvoc();
 
-        s.read_sensors();
-        auto co2 = s.get_co2();
-        auto tvoc = s.get_tvoc();
         auto hum = s2.measure_humidity();
         auto temp = s2.measure_temperature();
 
         std::cout << "T: " << std::fixed << std::setprecision(3) << temp;
         std::cout << "\tH: " << std::fixed << std::setprecision(3) << hum;
         std::cout << "\tC: " << dec << co2;
-        std::cout << "\tP: " << dec << s.get_tvoc() << endl;
+        std::cout << "\tO: " << dec << tvoc << endl;
 
-        this_thread::sleep_for(chrono::seconds(1));
+        s3.measure();
+        auto t2 = s3.get_temperature();
+        auto pres = s3.get_temperature();
+
+        std::cout << "T2: " << dec << t2 << "\tP: " << pres << std::endl;
+
+        this_thread::sleep_for(chrono::seconds(2));
     }
-#pragma clang diagnostic pop
 }
