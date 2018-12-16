68bc54e1551c0d909d4ed82ee90d61fbf03fdfc7
Onur Can Cakmak <occ@occ.me>
2018-12-16 01:24:23 -0500

    Calibrate CCS811 with measurements

--- main.cpp
+++ main.cpp
@@ -13,13 +13,19 @@ int main() {
         ccs811.read_sensors();
         bmp280.measure();
 
-        std::cout << "T(Si7021): " << std::fixed << std::setprecision(2) << si7021.measure_temperature() << "°C";
-        std::cout << "\tT(BMP280): " << std::fixed << std::setprecision(2) << bmp280.get_temperature() << "°C";
-        std::cout << "\tRH: " << std::fixed << std::setprecision(2) << si7021.measure_humidity() << "%";
+        float t_si7021 = si7021.measure_temperature();
+        double t_bmp20 = bmp280.get_temperature();
+        float relative_humidity = si7021.measure_humidity();
+
+        std::cout << "T(Si7021): " << std::fixed << std::setprecision(2) << t_si7021 << "°C";
+        std::cout << "\tT(BMP280): " << std::fixed << std::setprecision(2) << t_bmp20 << "°C";
+        std::cout << "\tRH: " << std::fixed << std::setprecision(2) << relative_humidity << "%";
         std::cout << "\tCO2: " << std::dec << ccs811.get_co2() << "ppm";
         std::cout << "\tTVOC: " << std::dec << ccs811.get_tvoc() << "ppm";
         std::cout << "\tPres: " << std::fixed << std::setprecision(2) << bmp280.get_pressure() << "hPa";
         std::cout << std::endl;
+
+        ccs811.set_env_data(relative_humidity, (t_si7021 + t_bmp20) / 2);
 
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
